2f35d24cd425e305453fa8c45bf94b6ab1ad585f
v_gol <tyurin.mihails@gmail.com>
2024-03-03 14:34:15 +0200

    ExerciseXP

---
+++ Week1/Day1/Daily Challenge Build Up A String/code.py
@@ -0,0 +1,23 @@
+user_string = input("Enter a string: ")
+if len(user_string) < 10:
+    print('string not long enough')
+elif len(user_string) > 10:
+    print('string too long')
+else:
+    print('perfect string')
+
+print(user_string[0],user_string[-1],sep='\n')
+
+print()
+
+string_out = ''
+for i in range(len(user_string)):
+    string_out += user_string[i]
+    print(string_out)
+
+import random as rd
+
+chars = list(user_string)
+rd.shuffle(chars)
+user_string_new = ''.join(chars)
+print(user_string_new)
