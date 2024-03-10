2109854ae253099e75ab98be01f6d0060fa2985f
v_gol <tyurin.mihails@gmail.com>
2024-03-10 10:21:12 +0200

    Advanced

--- Week1/Day5/Daily Challenge  Advanced Algorithm/Daily Challenge  Advanced Algorithm.py
+++ Week1/Day5/Daily Challenge  Advanced Algorithm/Daily Challenge  Advanced Algorithm.py
@@ -3,16 +3,21 @@ import random
 
 list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
 target_number = 3728
+unic_pair = set()
 
 def sum_couples(list_of_numbers, target_number):
-    cnt = 0
     flag = False
-    for i in range(0,len(list_of_numbers)-1):
-        if list_of_numbers[i] + list_of_numbers[i+1] == target_number:
-            cnt += 1
-            flag = True
+    for number in range(len(list_of_numbers)):
+        for i in range(len(list_of_numbers)):
+            if number + list_of_numbers[i] == target_number and i != number:
+                unic_pair.add(number)
+                unic_pair.add(list_of_numbers[i])
+                flag = True
     if flag == False:
         print('No solution at this generating')
     else:
-        print(f'There are {cnt} solutions at this generating')
+        print(f'There are {len(unic_pair)} solutions at this generating')
 sum_couples(list_of_numbers, 3728)
+
+
+
