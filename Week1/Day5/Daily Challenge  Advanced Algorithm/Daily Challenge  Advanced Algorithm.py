f4ea553187a4072e618e54845cd03a31716b2995
v_gol <tyurin.mihails@gmail.com>
2024-03-10 12:28:25 +0200

    Advanced

--- Week1/Day5/Daily Challenge  Advanced Algorithm/Daily Challenge  Advanced Algorithm.py
+++ Week1/Day5/Daily Challenge  Advanced Algorithm/Daily Challenge  Advanced Algorithm.py
@@ -3,21 +3,28 @@ import random
 
 list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
 target_number = 3728
-unic_pair = set()
 
 def sum_couples(list_of_numbers, target_number):
-    flag = False
-    for number in range(len(list_of_numbers)):
-        for i in range(len(list_of_numbers)):
-            if number + list_of_numbers[i] == target_number and i != number:
-                unic_pair.add(number)
-                unic_pair.add(list_of_numbers[i])
-                flag = True
-    if flag == False:
-        print('No solution at this generating')
-    else:
-        print(f'There are {len(unic_pair)} solutions at this generating')
-sum_couples(list_of_numbers, 3728)
+    unic_pair = {}
+    pairs_counter = 0
+    # make a dict with unic numbers as a key and the number of it in the list as a value
+    for number in list_of_numbers:
+        if number in unic_pair:
+            unic_pair[number] += 1
+        else:
+            unic_pair[number] = 1
+
+    for number in list_of_numbers:
+        complement = target_number - number
+        # if it is in dict
+        if complement in unic_pair:
+            pairs_counter += unic_pair[complement]
+        # do not count the same pair twice
+        if complement == number:
+            pairs_counter -= 1
+    print(pairs_counter//2)
+
+sum_couples(list_of_numbers,target_number)
 
 
 
