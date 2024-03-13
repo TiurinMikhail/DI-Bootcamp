5b1d0e899739d4c09922a42abc8bef181e90a7c9
v_gol <tyurin.mihails@gmail.com>
2024-03-13 10:04:16 +0200

    XP

--- Week2/Day3/Exercises XP/exercise_one.py
+++ Week2/Day3/Exercises XP/exercise_one.py
@@ -1,15 +1,4 @@
-import func
-
-#OR
-
-from func import add_two_number
-
-#OR
-
-from func import add_two_number as a2
-
-#OR
-
 import func as fn
 
-fn.add_two_number(5,4)
+fn.add_two_number(5, 4)
+fn.add_two_number(4, 8)
