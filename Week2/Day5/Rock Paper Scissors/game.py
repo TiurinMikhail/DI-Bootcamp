2f0c53eb38bd4f388fe58043cccf21f1975307c6
v_gol <tyurin.mihails@gmail.com>
2024-03-15 12:35:46 +0200

    Daily

---
+++ Week2/Day5/Rock Paper Scissors/game.py
@@ -0,0 +1,52 @@
+import random
+
+class Game:
+    game_items = ['rock', 'paper', 'scissors']
+
+    def get_user_item(self):
+        while True:
+            user_item = input("Select rock(r), paper(p) or scissors (s): ").lower()
+            letter_of_items = [i[0] for i in self.game_items]
+            if user_item == 'p':
+                user_item = 'paper'
+            elif user_item == 'r':
+                user_item = 'rock'
+            elif user_item == 's':
+                user_item = 'scissors'
+            if user_item in self.game_items:
+                return user_item
+            else:
+                print("Invalid input please try again. Your choice must be: rock(r), paper(p), or scissors(s)")
+
+    def get_computer_item(self):
+        computer_item = random.choice(self.game_items)
+        return computer_item
+
+    def get_game_result(self, user_item, computer_item):
+        # user_item = self.get_user_item()
+        # computer_item = self.get_computer_item()
+        if user_item == computer_item:
+            return "draw"
+        elif (user_item == "rock" and computer_item == 'scissors') or (user_item == "paper" and computer_item == 'rock') or (user_item == 'scissors' and computer_item == 'paper'):
+            return "win"
+        else:
+            return "loss"
+
+    def play(self):
+        user_item = self.get_user_item()
+        computer_item = self.get_computer_item()
+        game_result = self.get_game_result(user_item, computer_item)
+        if game_result == "draw":
+            print(f"You selected {user_item}. The computer selected {computer_item}. You drew!")
+            return game_result
+        elif game_result == "loss":
+            print(f"You selected {user_item}. The computer selected {computer_item}. You lose!")
+            return game_result
+        else:
+            print(f"You selected {user_item}. The computer selected {computer_item}. You won!")
+            return game_result
+
+#
+# game1 = Game()
+#
+# game1.play()
---
+++ Week2/Day5/Rock Paper Scissors/rock-paper-scissors.py
@@ -0,0 +1,39 @@
+from game import Game
+
+
+def get_user_menu_choice():
+    print('     MENU\n', '(g) Play a new game\n', '(x) Show scores\n', '(q) Quit\n')
+    valid_input = ['g', 'x', 'q']
+    while True:
+        user_input = input('Enter your choice: ').lower()
+        if user_input in valid_input:
+            return user_input
+        else:
+            print("Invalid choice. Please enter 'g', 'x', or 'q'.")
+
+
+def print_results(results):
+    for key, value in results.items():
+        print(f'{key} : {value}')
+
+
+def main():
+    results = {"win": 0, "loss": 0, "draw": 0}
+    while True:
+        user_input = get_user_menu_choice()
+        if user_input == 'g':
+            user_game = Game()
+            result = user_game.play()
+            results[result] += 1
+
+        elif user_input == 'x':
+            print_results(results)
+
+        elif user_input == 'q':
+            print('Game Results:\n')
+            print(f'You won {results["win"]} times')
+            print(f'You lost {results["loss"]} times')
+            print(f'You drew {results["draw"]} times')
+            break
+
+main()
