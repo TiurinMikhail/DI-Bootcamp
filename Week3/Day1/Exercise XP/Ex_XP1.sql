6729eb986953bdec4ffe55c5406a927b1db3660f
v_gol <tyurin.mihails@gmail.com>
2024-03-17 13:19:29 +0200

    Ex_XP

---
+++ Week3/Day1/Exercise XP/Ex_XP1.sql
@@ -0,0 +1,64 @@
+-- Database: public
+
+-- DROP DATABASE IF EXISTS public;
+
+-- CREATE DATABASE public
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'Russian_Russia.936'
+--     LC_CTYPE = 'Russian_Russia.936'
+--     LOCALE_PROVIDER = 'libc'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
+
+-- create table items(
+-- item_id serial primary key,
+-- item_name varchar(50) not null,
+-- item_price integer not null
+-- );
+
+
+-- create table customers(
+-- customer_id serial primary key,
+-- first_name varchar(50) not null,
+-- last_name varchar(100) not null
+-- );
+
+-- insert into items(item_name,item_price)
+-- values ('Small Desk',100),
+-- ('Large desk',300),
+-- ('Fan',80);
+
+-- insert into customers(first_name,last_name)
+-- values ('Greg','Jones'),
+-- ('Sandra','Jones'),
+-- ('Scott','Scott'),
+-- ('Trevor','Green'),
+-- ('Melanie','Johnson');
+
+
+
+-- select*
+-- from items;
+
+-- select*
+-- from items
+-- where item_price > 80;
+
+-- select*
+-- from items
+-- where item_price <= 300;
+
+
+-- select*
+-- from customers 
+-- where last_name = 'Jones'
+
+-- select*
+-- from customers 
+-- where first_name != 'Scott'
+
+
+
